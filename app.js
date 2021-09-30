6db246d1e7275e79cba1be9fcff34b228df3f3c9
José Ricardo <josermca01@gmail.com>
2021-09-30 19:46:24 -0300

    barcos na grid da maquina

--- app.js
+++ app.js
@@ -25,4 +25,67 @@ document.addEventListener('DOMContentLoaded',()=>{
 
     board(gridplayer,playersqures)
     board(gridmaquina,maquinasquares)
+
+    //Barcos
+    const arraybarcos = [
+        {
+            name : 'destroyer',
+            directions : [
+                [0,1],
+                [0,tamanho]
+            ]
+        },
+        {
+            name : 'submarino',
+            directions : [
+                [0,1,2],
+                [0,tamanho,tamanho*2]
+            ]
+        },
+        {
+            name : 'cruzador',
+            directions : [
+                [0,1,2],
+                [0,tamanho,tamanho*2]
+            ]
+        },
+        {
+            name : 'navio',
+            directions : [
+                [0,1,2,3],
+                [0,tamanho,tamanho*2,tamanho*3]
+            ]
+        },
+        {
+            name : 'portaavioes',
+            directions : [
+                [0,1,2,3,4],
+                [0,tamanho,tamanho*2,tamanho*3,tamanho*4]
+            ]
+        }
+    ]
+
+    //criando grid da maquina
+    function generate(ship) {
+        let randomDirection = Math.floor(Math.random() * ship.directions.length)
+        let current = ship.directions[randomDirection]
+        if (randomDirection === 0) direction = 1
+        if (randomDirection === 1) direction = 10
+        let randomStart = Math.abs(Math.floor(Math.random() * maquinasquares.length - (ship.directions[0].length * direction)))
+    
+        const isTaken = current.some(index => maquinasquares[randomStart + index].classList.contains('taken'))
+        const isAtRightEdge = current.some(index => (randomStart + index) % tamanho === tamanho)
+        const isAtLeftEdge = current.some(index => (randomStart + index) % tamanho === 0)
+    
+        if (!isTaken && !isAtRightEdge && !isAtLeftEdge) current.forEach(index => maquinasquares[randomStart + index].classList.add('taken', ship.name))
+    
+        else generate(ship)
+      }
+
+      generate(arraybarcos[0])
+      generate(arraybarcos[1])
+      generate(arraybarcos[2])
+      generate(arraybarcos[3])
+      generate(arraybarcos[4])
+
 })
